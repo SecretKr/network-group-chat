ad29c59e94ddf4e463471e57834adda1dc78f158
SecretKr <kraiwit.wit@gmail.com>
2025-04-13 11:01:44 +0700

    chore: auto gen api

--- server/src/server.js
+++ server/src/server.js
@@ -3,9 +3,12 @@ import express from "express";
 import { Server } from "socket.io";
 import dotenv from "dotenv";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import fs from "fs";
+const swaggerDocument = JSON.parse(fs.readFileSync("./swagger-output.json"));
 import connectDB from "./database/db.js";
 import auth from "./routes/auth.js";
-import chat from "./routes/chat.js";  
+import chat from "./routes/chat.js";
 
 dotenv.config();
 connectDB();
@@ -13,6 +16,7 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(
   cors({
     origin: "*",
@@ -20,7 +24,6 @@ app.use(
     allowedHeaders: ["Content-Type"],
   })
 );
-
 app.use(express.json());
 app.use("/api/v1/auth", auth);
 app.use("/api/v1/chat", chat);
