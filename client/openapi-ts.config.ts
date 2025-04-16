d9a3b32815100e97d49b23d3ea6985697c054ee9
SecretKr <kraiwit.wit@gmail.com>
2025-04-16 13:30:06 +0700

    fix: env for hey-api base url

--- client/openapi-ts.config.ts
+++ client/openapi-ts.config.ts
@@ -3,5 +3,10 @@ import { defineConfig } from "@hey-api/openapi-ts";
 export default defineConfig({
   input: "../server/swagger-output.json",
   output: "src/generated/api",
-  plugins: ["@hey-api/client-fetch"],
+  plugins: [
+    {
+      name: "@hey-api/client-fetch",
+      runtimeConfigPath: "./src/lib/api-client.ts",
+    },
+  ],
 });
